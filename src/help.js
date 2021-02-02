d1ae846b5e1b8cc2b8182f973ef975c5702c3fd9
NICKZZIN <76818604+NICKZZIN@users.noreply.github.com>
2021-02-02 20:05:34 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,177 @@
+const help = (prefix) => {
+	return `
+「 *𝙉𝙄𝘾𝙆𝙔 - 𝙉𝙄𝘽𝙊𝙏* 」
+
+ ♤ *informações*
+ ♤ Prefix: 「  ${prefix}  」
+ ♤ Criador : 𝘕𝘪𝘤𝘬𝘻𝘻𝘪𝘯 𝘎𝘰𝘥
+ ♤ 𝘔𝘌𝘜 𝘊𝘈𝘕𝘈𝘓 𝘋𝘈 𝘜𝘔𝘈 𝘍𝘖𝘙𝘊𝘈 𝘓𝘈 𝘍𝘔𝘓 https://youtu.be/D58GrbAC1fA
+ ♤ Link do meu grupo: https://chat.whatsapp.com/HrXtjnWdN1O72ZDeG0PZH3
+❖ ── ✦ ──『✙』── ✦ ── ❖
+♤ 𝙊𝙪𝙩𝙧𝙤𝙨 𝙈𝙚𝙣𝙪𝙨
+*╔═══❖•ೋ° °ೋ•❖═══╗*
+☛ menuadmin
+☛ nsfwmenu
+☛ paulomenu
+*╚═══❖•ೋ° °ೋ•❖═══╝*
+ ❖ ── ✦ ──『✙』── ✦ ── ❖
+                       𝙎𝙤𝙗𝙧𝙚
+  ▋
+  ▋▰ ☛ ${prefix}info
+  ▋▰ ☛ ${prefix}blocklist
+  ▋▰ ☛ ${prefix}chatlist
+  ▋▰ ☛ ${prefix}ping
+  ▋▰ ☛ ${prefix}bugreport
+ ❖ ── ✦ ──『✙』── ✦ ── ❖
+                        𝙁𝙖𝙯𝙚𝙧
+  ▋
+  ▋▰ ☛ ${prefix}sticker
+  ▋▰ ☛ ${prefix}stickergif
+  ▋▰ ☛ ${prefix}toimg
+  ▋▰ ☛ ${prefix}tomp3
+  ▋▰ ☛ ${prefix}bpink
+  ▋▰ ☛ ${prefix}marvellogo
+  ▋▰ ☛ ${prefix}snowwrite
+  ▋▰ ☛ ${prefix}3dtext
+  ▋▰ ☛ ${prefix}ninjalogo
+  ▋▰ ☛ ${prefix}water
+  ▋▰ ☛ ${prefix}firetext
+  ▋▰ ☛ ${prefix}logowolf
+  ▋▰ ☛ ${prefix}logowolf2
+  ▋▰ ☛ ${prefix}phlogo
+  ▋▰ ☛ ${prefix}glitch
+  ▋▰ ☛ ${prefix}neonlogo
+  ▋▰ ☛ ${prefix}neonlogo2
+  ▋▰ ☛ ${prefix}lionlogo
+  ▋▰ ☛ ${prefix}jokerlogo
+  ▋▰ ☛ ${prefix}shadow
+  ▋▰ ☛ ${prefix}burnpaper
+  ▋▰ ☛ ${prefix}coffee
+  ▋▰ ☛ ${prefix}lovepaper
+  ▋▰ ☛ ${prefix}woodblock
+  ▋▰ ☛ ${prefix}qowheart
+  ▋▰ ☛ ${prefix}mutgrass
+  ▋▰ ☛ ${prefix}undergocean
+  ▋▰ ☛ ${prefix}woodenboards
+  ▋▰ ☛ ${prefix}wolfmetal
+  ▋▰ ☛ ${prefix}metalictglow
+  ▋▰ ☛ ${prefix}8bit
+  ▋▰ ☛ ${prefix}ttp
+  ▋▰ ☛ ${prefix}herrypotter
+  ▋▰ ☛ ${prefix}pubglogo
+  ▋▰ ☛ ${prefix}quotemaker
+ ❖ ── ✦ ──『✙』── ✦ ── ❖
+                        𝙈𝙞𝙙𝙞𝙖
+  ▋
+  ▋▰ ☛ ${prefix}trendtwit
+  ▋▰ ☛ ${prefix}randomkpop
+  ▋▰ ☛ ${prefix}ytsearch
+ ❖ ── ✦ ──『✙』── ✦ ── ❖
+                    𝙀𝙙𝙪𝙘𝙖𝙘̧𝙖̃𝙤
+  ▋
+  ▋▰ ☛ ${prefix}wiki
+  ▋▰ ☛ ${prefix}wikien
+  ▋▰ ☛ ${prefix}nulis
+  ▋▰ ☛ ${prefix}quotes
+  ▋▰ ☛ ${prefix}quotes2
+  ▋▰ ☛ ${prefix}artinama
+ ❖ ── ✦ ──『✙』── ✦ ── ❖
+               𝘾𝙤𝙣𝙘𝙝𝙖𝙨 𝙈𝙖́𝙜𝙞𝙘𝙖𝙨
+  ▋
+  ▋▰ ☛ ${prefix}apakah
+  ▋▰ ☛ ${prefix}kapankah
+  ▋▰ ☛ ${prefix}rate
+  ▋▰ ☛ ${prefix}bisakah
+ ❖ ── ✦ ──『✙』── ✦ ── ❖
+                   𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙𝙚𝙧
+  ▋▰
+  ▋▰ ☛ ${prefix}images
+  ▋▰ ☛ ${prefix}ytmp3
+  ▋▰ ☛ ${prefix}ytmp4
+  ▋▰ ☛ ${prefix}tiktok
+  ▋▰ ☛ ${prefix}joox
+ ❖ ── ✦ ──『✙』── ✦ ── ❖
+                        𝙈𝙚𝙢𝙚
+  ▋
+  ▋▰ ☛ ${prefix}meme
+  ▋▰ ☛ ${prefix}memeindo
+ ❖ ── ✦ ──『✙』── ✦ ── ❖
+                          𝙎𝙤𝙢
+  ▋
+  ▋▰ ☛ ${prefix}play
+  ▋▰ ☛ ${prefix}tts
+ ❖ ── ✦ ──『✙』── ✦ ── ❖
+                        𝙈𝙪𝙨𝙞𝙘𝙖
+  ▋
+  ▋▰ ☛ ${prefix}lirik
+  ▋▰ ☛ ${prefix}chord
+ ❖ ── ✦ ──『✙』── ✦ ── ❖
+                     𝙄𝙨𝙡𝙖𝙢𝙞𝙨𝙢𝙤𝙨
+  ▋
+  ▋▰ ☛ ${prefix}quran
+ ❖ ── ✦ ──『✙』── ✦ ── ❖
+                        𝙎𝙩𝙖𝙡𝙠𝙚𝙖𝙧
+  ▋ 
+  ▋▰ ☛ ${prefix}tiktokstalk
+  ▋▰ ☛ ${prefix}igstalk
+ ❖ ── ✦ ──『✙』── ✦ ── ❖
+                         𝙒𝙞𝙗𝙪 
+  ▋
+  ▋▰ ☛ ${prefix}neonime
+  ▋▰ ☛ ${prefix}pokemon
+  ▋▰ ☛ ${prefix}loli
+  ▋▰ ☛ ${prefix}waifu
+  ▋▰ ☛ ${prefix}randomanime
+  ▋▰ ☛ ${prefix}husbu
+  ▋▰ ☛ ${prefix}husbu2
+  ▋▰ ☛ ${prefix}wait
+  ▋▰ ☛ ${prefix}nekonime
+ ❖ ── ✦ ──『✙』── ✦ ── ❖
+                       𝘿𝙞𝙫𝙚𝙧𝙨𝙖̃𝙤
+  ▋
+  ▋▰ ☛ ${prefix}alay
+  ▋▰ ☛ ${prefix}gantengcek
+  ▋▰ ☛ ${prefix}watak
+  ▋▰ ☛ ${prefix}hobby
+  ▋▰ ☛ ${prefix}game
+  ▋▰ ☛ ${prefix}bucin
+  ▋▰ ☛ ${prefix}trust
+  ▋▰ ☛ ${prefix}dare
+  ▋▰ ☛ ${prefix}simi
+ ❖ ── ✦ ──『✙』── ✦ ── ❖
+                   𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙘̧𝙖̃𝙤
+  ▋
+  ▋▰ ☛ ${prefix}bahasa
+  ▋▰ ☛ ${prefix}kodenegara
+  ▋▰ ☛ ${prefix}kbbi
+  ▋▰ ☛ ${prefix}fakta
+  ▋▰ ☛ ${prefix}infocuaca
+  ▋▰ ☛ ${prefix}infogempa
+  ▋▰ ☛ ${prefix}jadwaltvnow
+  ▋▰ ☛ ${prefix}covid
+ ❖ ── ✦ ──『✙』── ✦ ── ❖
+                        𝘿𝙤𝙣𝙤
+  ▋
+  ▋▰ ☛ ${prefix}setprefix
+  ▋▰ ☛ ${prefix}block
+  ▋▰ ☛ ${prefix}bc
+  ▋▰ ☛ ${prefix}bcgc
+  ▋▰ ☛ ${prefix}clone
+  ▋▰ ☛ ${prefix}clearall
+ ❖ ── ✦ ──『✙』── ✦ ── ❖
+                       𝙊𝙪𝙩𝙧𝙤𝙨
+  ▋
+  ▋▰ ☛ ${prefix}send
+  ▋▰ ☛ ${prefix}wame
+  ▋▰ ☛ ${prefix}virtex
+  ▋▰ ☛ ${prefix}exe
+  ▋▰ ☛ ${prefix}qrcode
+  ▋▰ ☛ ${prefix}afk
+  ▋▰ ☛ ${prefix}timer
+  ▋▰ ☛ ${prefix}fml
+  ▋▰ ☛ ${prefix}fml2
+❖ ── ✦ ──『✙』── ✦ ── ❖
+`
+}
+
+exports.help = help
---
+++ src/nsfwmenu.js
@@ -0,0 +1,23 @@
+const nsfwmenu = (prefix, pushname) => {
+    return `slv resumindo aqui e o menu NSFW
+    ◪ *NSFW*
+  │
+  ├─ ❏ ${prefix}nsfwbobs
+  ├─ ❏ ${prefix}randomhentaio
+  ├─ ❏ ${prefix}nsfwtrap
+  ├─ ❏ ${prefix}nsfwass
+  ├─ ❏ ${prefix}nsfwbelly
+  ├─ ❏ ${prefix}nsfwsidebobs
+  ├─ ❏ ${prefix}nsfwahegao
+  ├─ ❏ ${prefix}nsfwthighs
+  ├─ ❏ ${prefix}nsfwarmpits
+  └─ ❏ ${prefix}nsfwfeets
+
+bobs = peitos\nrandom hentai = rentai aleatório\ntrap = trans😏👺\nass = bundas\nbelly = descobre ai crtz q vc gosta delX🤭😏\nsidebobs = peitos "tortos" sinceramente não gostei muito\nahegao = ahegao neh dãr\nthings = coisas estranhas\narmpits = axilas\nfeets = pézinhus
+  _obs para bloquear esses comandos o menu NSFW_\n _Digite_\n ${prefix}nsfw 0`
+
+
+
+}
+
+exports.nsfwmenu = nsfwmenu
