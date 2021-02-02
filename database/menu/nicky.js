cb5befa18459e50e1e047435054bb14b88567ee5
NICKZZIN <76818604+NICKZZIN@users.noreply.github.com>
2021-02-02 20:28:17 -0300

    Add files via upload

---
+++ database/menu/nicky.js
@@ -0,0 +1,13 @@
+const nicky = (prefix, ownerBot, botName) => {
+        return `
+「 *${botName}* 」
+
+◪ INFO
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Creator: ${ownerBot}
+  │
+  ├─ ❏ O Nome do bot: Nicky
+  ├─ ❏NICKZZIN O MAIS BRABO
+  └─ `
+}
+exports.nicky = nicky
