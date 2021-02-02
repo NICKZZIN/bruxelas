eb3055eb7e20abafb5ef6358aece6fa211e6deb0
NICKZZIN <76818604+NICKZZIN@users.noreply.github.com>
2021-02-02 20:09:20 -0300

    Add files via upload

---
+++ src/nimenu.js
@@ -0,0 +1,12 @@
+const nimenu = (prefix, pushname) => {
+    return `◪ *Comandos do 𝙉𝙄𝘽𝙊𝙏*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.nimenu = nimenu
